ca1f825d4f9502600c56084c0f97a67bc0c66aa4
nazazer <nazazermik@gmail.com>
2024-07-30 18:38:08 +0100

    Quick Admin Panel done

--- src/routes/admin-auth.tsx
+++ src/routes/admin-auth.tsx
@@ -1,5 +1,31 @@
-import { createFileRoute } from "@tanstack/react-router";
+import React from "react";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import "../styles/auth.scss";
 
 export const Route = createFileRoute("/admin-auth")({
-  component: () => <div>Hello /admin-auth!</div>,
+  component: AdminLogin,
 });
+
+function AdminLogin() {
+  return (
+    <>
+      <div className="auth-container">
+        <div className="right-panel">
+          <Link to={"/"}>
+            <h2>Polestar</h2>
+          </Link>
+          <h1>Sign in to Admin Panel</h1>
+          <form id="admin-auth">
+            <label htmlFor="admin-username">Username</label>
+            <input type="email" id="admin-username" maxLength={30} />
+            <label htmlFor="admin-password">Password</label>
+            <input type="password" id="admin-password" maxLength={30} />
+            <center>
+              <button type="submit">Sign in</button>
+            </center>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
